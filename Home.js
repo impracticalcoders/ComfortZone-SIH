5e295eb7215077f1f44272fffe1434a5b621312b
saiaakash <p.saiaakash518@gmail.com>
2019-11-26 22:36:57 +0530

    added logo to start page

--- Home.js
+++ Home.js
@@ -1,16 +1,17 @@
 
 import React from 'react';
-import {Text,View,Button} from 'react-native'
-
+import {Text,View,Button,Image} from 'react-native'
+import logo from './images/logo.jpg'
 const Home= (props)=>{
   
   return(
-    <View style={{height:"100%",alignItems:'center'/*for horizontal alignment*/ }}>   
+    <View style={{height:"100%",marginTop:20,alignItems:'center'/*for horizontal alignment*/ }}>   
        <Text style={{fontSize:30}}>Comfort Zone</Text>
-
-       <View style={{justifyContent:'center',height:'100%'}}>
-         <Button title="Sign up" onPress={()=>props.navigation.navigate('MCS')}  />
-       </View>
+        <Image source ={logo} style={{width: 300, height: 300,marginTop:30}}/>
+        
+        <View style={{marginTop:30}}>
+         <Button title="Continue" onPress={()=>props.navigation.navigate('SignIn')}  />
+         </View>
        
         </View> 
   
---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from "react-native"
+const Dashboard = () => {
+    return (
+        <View>
+            <Text>
+            hello world
+            </Text>
+
+        </View>
+    );
+};
+
+export default Dashboard;
