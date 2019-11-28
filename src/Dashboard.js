d22ca7cc7ab12db6daf686874f06d454bd56719d
saiaakash <p.saiaakash518@gmail.com>
2019-11-28 18:43:41 +0530

    added simple text box

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import {Text,View,Button} from "react-native"
+import {Text,View,Button,TextInput,StyleSheet} from "react-native"
 const Dashboard = () => {
 
+    const styles = StyleSheet.create({
+        parent:{
+            height:"90%",
+            width:"100%"
+        },
+        InputText:{
+            borderColor:'#d6d7da',
+            borderWidth:1,
+            maxHeight:'90%',
+            fontSize:20
+        }
+    })
    
     return (
         
-        <View style={{height:"90%",width:"100%"}}>
-            <Text>hllo</Text>
+        <View style={styles.parent}>
+            
+            <TextInput 
+            style={styles.InputText}
+            placeholder='How are you feeling today?' 
+            multiline
+            />
+            <Button title="LOG"/>
         </View>
         
     );
