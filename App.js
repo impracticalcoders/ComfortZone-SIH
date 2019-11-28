d7284b3f39b66343940d3f5ade1442bb30819695
saiaakash <p.saiaakash518@gmail.com>
2019-11-28 18:20:38 +0530

    cleaned code

--- App.js
+++ App.js
@@ -2,28 +2,13 @@
 import React from 'react';
 import {View,Button} from 'react-native'
 import Home from './Home.js'
-import Home2 from './Home2.js'
-import MC from './MC'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Dashboard from './src/Dashboard.js';
 
-const App= ()=>{
-  
-  return(
-    <View >   
-     <Button title="HOme" onPress={()=>{
-
-     }} />
-    </View> 
-  
-  )
-}
 
 const AppNavigator = createStackNavigator({
   HomeS:Home,
-  Home2S:Home2,
-  SignIn:MC,
   dashboard:Dashboard
 });
 
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import {Text,View} from "react-native"
+import React, { useState, useEffect } from 'react';
+import {Text,View,Button} from "react-native"
 const Dashboard = () => {
-    return (
-        <View>
-            <Text>
-            hello world
-            </Text>
 
+   
+    return (
+        
+        <View style={{height:"90%",width:"100%"}}>
+            <Text>hllo</Text>
         </View>
+        
     );
 };
 
