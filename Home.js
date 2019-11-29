b117cbaab299ad097fb3395a4aa695d4c545ba67
saiaakash <p.saiaakash518@gmail.com>
2019-11-29 23:18:10 +0530

    added Diary with the search algo

--- Home.js
+++ Home.js
@@ -1,19 +1,36 @@
 
 import React from 'react';
-import {Text,View,Button,Image} from 'react-native'
+import {Text,View,Button,Image,StyleSheet} from 'react-native'
 import logo from './images/logo.jpg'
-import {GiftedChat} from "react-native-gifted-chat"
 
-const Home= (props)=>{
+function Home(props){
   
+  const styles = StyleSheet.create({
+      parent:{
+        height:"100%",
+        marginTop:20,
+        alignItems:'center'/*for horizontal alignment*/ 
+      },
+      title:{
+        fontSize:30
+      },
+      logo:{
+        width: 300,
+        height: 300,
+        marginTop:30
+      },
+      continue:{marginTop:30}
+
+  })
+   
   return(
-    <View style={{height:"100%",marginTop:20,alignItems:'center'/*for horizontal alignment*/ }}>   
-       <Text style={{fontSize:30}}>Comfort Zone</Text>
-        <Image source ={logo} style={{width: 300, height: 300,marginTop:30}}/>
-        
-        <View style={{marginTop:30}}>
-         <Button title="Continue" onPress={()=>props.navigation.navigate('dashboard')}  />
-         </View>
+
+    <View style={styles.parent}>   
+       <Text style={styles.title}>Comfort Zone</Text>
+        <Image source ={logo} style={styles.logo}/>
+        <View style={styles.continue}>
+            <Button title="Continue" onPress={()=>props.navigation.navigate('dashboard')}  />
+        </View>
        
         </View> 
 
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,32 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import {Text,View,Button,TextInput,StyleSheet} from "react-native"
-const Dashboard = () => {
+import {Text,View,Button,TextInput,StyleSheet,ScrollView} from "react-native"
+
+function Dashboard ()  {
+    const [musicList,setmusicList] = useState([])
+    const [searchItem,setsearchItem] = useState('')
+
+    useEffect(()=>{
+        if(searchItem)
+        fetch("https://api.spotify.com/v1/search?q="+searchItem+"&type=playlist",{
+            headers: {
+            'Authorization': 'Bearer ' + 'BQDawofAh5oK4kSxLIRJdz5tmCX4e-GRGTCB90Q7qgv-kEQ6IRPSJ-vR-1wwuzpw7ul7y2O1WTdITxP1KGBFKp34NxRs5Enn4g6Mf_lSWrXiqmXXw2ea8atcFBpcmFuBzn8M702IYqWXxraCTCtxuY4E0rDri91KeYALHbFFnoj7si_RnYOhmYA'
+            }
+        }).then(res=>res.json()).then(resp=>{
+            let list = []
+            for(let i in resp.playlists.items )
+            {
+                list.push(resp.playlists.items[i].name)
+            }
+            setmusicList(list)
+            
+
+        })
+    })
 
     const styles = StyleSheet.create({
         parent:{
             height:"90%",
-            width:"100%"
+            width:"100%",
+            alignItems:'center'
         },
         InputText:{
             borderColor:'#d6d7da',
             borderWidth:1,
+            borderRadius:15,
+            marginTop:20,
             maxHeight:'90%',
+            width:'95%',
             fontSize:20
-        }
+        },
+        logbtn:{
+            marginTop:20
+        },
+       
     })
    
     return (
         
         <View style={styles.parent}>
-            
             <TextInput 
-            style={styles.InputText}
-            placeholder='How are you feeling today?' 
-            multiline
+                style={styles.InputText}
+                placeholder='How are you feeling today?' 
+                multiline
+                onChangeText={(text)=>setsearchItem(text)}
             />
-            <Button title="LOG"/>
+            <View style={styles.logbtn}>
+                <Button title="LOG" />
+            </View>
+
+            <ScrollView>
+                <Text style={{fontSize:20}}> {musicList.map(item=>item+'\n')}</Text>
+             </ScrollView>
+
         </View>
-        
+    
     );
 };
 
