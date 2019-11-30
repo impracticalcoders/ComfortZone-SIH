c26fb57fc108113cb4605c286e77c82553f1dfe5
psk907 <p.surajkumar907@gmail.com>
2019-11-30 12:39:22 +0530

    set to work

--- Home.js
+++ Home.js
@@ -8,7 +8,7 @@ const Home= (props)=>{
   
   return(
     <View style={{height:"100%",marginTop:20,alignItems:'center'/*for horizontal alignment*/ }}>   
-       <Text style={{fontSize:30}}>Comfort Zone</Text>
+       <Text style={{fontSize:30}}>SmileAWhile</Text>
         <Image source ={logo} style={{width: 300, height: 300,marginTop:30}}/>
         
         <View style={{marginTop:30}}>
