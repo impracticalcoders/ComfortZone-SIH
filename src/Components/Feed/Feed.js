7399fb4b415bb600951343bb8b2fd89afc05c542
saiaakash <p.saiaakash518@gmail.com>
2019-12-11 22:16:10 +0530

    added boilerplate for feed

---
+++ src/Components/Feed/Feed.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+import {ScrollView,Text,View} from "react-native"
+import FeedItem from "./FeedItem"
+function feed(){
+    return(
+    <ScrollView>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+        <FeedItem/>
+    </ScrollView>)
+}
+
+export default feed
---
+++ src/Components/Feed/FeedItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import {ScrollView,Text,View,} from "react-native"
+
+function FeedItem(){
+    return(
+    <View>
+        <Text style={{textAlign:'center',padding:20}}>hello this is your feed item</Text>    
+    </View>)
+}
+
+export default FeedItem
