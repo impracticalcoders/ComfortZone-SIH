d92ed5d8f95cceb3f1b73f099661263e6bc519f6
saiaakash <p.saiaakash518@gmail.com>
2019-12-14 10:06:02 +0530

    structured code and replaced textbox with button

--- Home.js
+++ Home.js
@@ -1,17 +1,19 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import {
-  ScrollView,
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  StatusBar,
-  Image,
-  TouchableOpacity,
+    ScrollView,
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Easing,
+    StatusBar,
+    Image,
+    TouchableOpacity,
+    Animated,
+    Dimensions
 } from 'react-native';
-
+import {Button} from "react-native-elements"
 import diarylogo from './images/diaryicon.jpg';
 import lifesaver from './images/lifesaver.png';
 import homeicon from './images/homeicon.png';
@@ -21,143 +23,185 @@ import users from './images/users.png';
 import Feed from "./src/Components/Feed/Feed.js"
 
 function Home(props) {
-  const styles = StyleSheet.create({
-    container: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#f7e892',
-    },
-    parent: {
-      height: '100%',
-      marginTop: 20,
-      alignItems: 'center',
-    },
-    title: {
-      fontSize: 30,
-    },
-    logo: {
-      marginTop: 50,
-      height: 400,
-      width: '100%',
-      opacity: 1,
-    },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-      marginBottom: 50,
-      color: 'black',
-    },
-    MotivationalQuote: {
-      padding: 20,
-      top: 20,
-    },
-    buttons: {
-      marginVertical: 4,
-      marginHorizontal: 4,
-      height: 40,
-      backgroundColor: 'white',
-      alignItems: 'center',
-    },
-    
-    header: {
-      top: 0,
-      bottom: 90,
-    },
-  });
 
-  return (
-    <View>
+    const styles = StyleSheet.create({
+        container: {
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#f7e892'
+        },
+        parent: {
+            height: '100%',
+            marginTop: 20,
+            alignItems: 'center'
+        },
+        title: {
+            fontSize: 30
+        },
+        logo: {
+            marginTop: 50,
+            height: 400,
+            width: '100%',
+            opacity: 1
+        },
+        welcome: {
+            fontSize: 20,
+            textAlign: 'center',
+            margin: 10,
+            marginBottom: 50,
+            color: 'black'
+        },
+        MotivationalQuote: {
+            padding: 20,
+            top: 20
+        },
+        buttons: {
+            marginVertical: 4,
+            marginHorizontal: 4,
+            height: 40,
+            backgroundColor: 'white',
+            alignItems: 'center'
+        },
 
+        header: {
+            top: 0,
+            bottom: 90
+        }
+    });
+    const screenHeight = Math.round(Dimensions.get('window').height);
+    const screenWidth = Math.round(Dimensions.get('window').width);
 
+    return (
+        <View>
 
-    {/* Online Offline */}
-      <View
-        style={{
-          marginTop:10,
-          flex: 1,
-          flexDirection: 'row',
-        }}>
-        <View style={{width: '50%'}}>
-          <TouchableOpacity style={styles.buttons}>
-            <Text style={{textAlign: 'center', fontSize: 15}}> Online</Text>
-          </TouchableOpacity>
+            <View
+                style={{
+                    marginTop: 10,
+                    flex: 1,
+                    flexDirection: 'row'
+                }}>
+                <View style={{
+                        width: '50%'
+                    }}>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text
+                            style={{
+                                textAlign: 'center',
+                                fontSize: 15
+                            }}>
+                            Online</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{
+                        width: '50%'
+                    }}>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text
+                            style={{
+                                textAlign: 'center',
+                                fontSize: 15
+                            }}>
+                            Offline</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View
+                style={{
+                    marginTop: 30,
+                    marginBottom: 10,
+                    marginLeft: 10,
+                    marginRight: 10
+                }}>
+                <Button
+                    style={{
+                        fontSize: 20,
+                        borderColor: 'gray',
+                        borderWidth: 2,
+                        marginTop: 10,
+                        marginBottom: -10,
+                        padding: 10,
+                        borderRadius: 30
+                    }}
+                    onPress={() => alert('do not touch me')}
+                    title='Express your feelings here'></Button>
+
+            </View>
+
+            <View style={{
+                    height: "78%"
+                }}>
+                {/* <Image source={logo} style={styles.logo} /> */}
+                <Feed/>
+            </View>
+
+          
+            <View ></View>
+            <View
+                style={{
+                    flex: 1,
+                    flexDirection: 'row'
+                }}>
+                <View style={{
+                        width: '25%'
+                    }}>
+                    <TouchableOpacity
+                        style={styles.buttons}
+                        onPress={() => props.navigation.navigate('media')}>
+                        <Image
+                            source={homeicon}
+                            style={{
+                                width: 40,
+                                height: 40
+                            }}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={{
+                        width: '25%'
+                    }}>
+                    <TouchableOpacity
+                        style={styles.buttons}
+                        onPress={() => props.navigation.navigate('prevnotes')}>
+                        <Image
+                            source={diarylogo}
+                            style={{
+                                width: 40,
+                                height: 40
+                            }}/>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{
+                        width: '25%'
+                    }}>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Image
+                            source={users}
+                            style={{
+                                width: 40,
+                                height: 40
+                            }}/>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{
+                        width: '25%'
+                    }}>
+                    <TouchableOpacity
+                        style={styles.buttons}
+                        onPress={() => props.navigation.navigate('emergency')}>
+                        <Image
+                            source={lifesaver}
+                            style={{
+                                width: 35,
+                                height: 35
+                            }}/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
         </View>
-        <View style={{width: '50%'}}>
-          <TouchableOpacity style={styles.buttons}>
-            <Text style={{textAlign: 'center', fontSize: 15}}> Offline</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-
-      {/* text box */}
-      
-      <View style={{marginTop:30,marginBottom:10}}>
-      <TextInput multiline={true} style={{fontSize:20,borderColor:'gray',borderWidth:2,marginTop:10,marginBottom:-10,padding:10}} placeholder='enter your feelings'>hel</TextInput>  
-      </View>
-    
-
-
-
-    {/* image and feed */}
-    <View style={{height:"78%"}}>
-    
-      
-      {/* <Image source={logo} style={styles.logo} /> */}
-
-      <Feed/>
-      
-     
-    </View>
-
-
-
-
-    {/* Navbar */}
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-        }}>
-        <View style={{width: '25%'}}>
-          <TouchableOpacity
-            style={styles.buttons}
-            onPress={() => props.navigation.navigate('media')}>
-            <Image source={homeicon} style={{width: 40, height: 40}} />
-          </TouchableOpacity>
-        </View>
-        <View style={{width: '25%'}}>
-          <TouchableOpacity
-            style={styles.buttons}
-            onPress={() => props.navigation.navigate('prevnotes')}>
-            <Image source={diarylogo} style={{width: 40, height: 40}} />
-          </TouchableOpacity>
-        </View>
-
-        <View style={{width: '25%'}}>
-          <TouchableOpacity style={styles.buttons}>
-            <Image source={users} style={{width: 40, height: 40}} />
-          </TouchableOpacity>
-        </View>
-
-        <View style={{width: '25%'}}>
-          <TouchableOpacity
-            style={styles.buttons}
-            onPress={() => props.navigation.navigate('emergency')}>
-            <Image source={lifesaver} style={{width: 35, height: 35}} />
-          </TouchableOpacity>
-        </View>
-      </View>
-    
-    
-
-  
-    
-    
-    
-    </View>
-  );
+    );
 }
 
 export default Home;
--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,53 +1,76 @@
-import React,{useState} from "react"
+import React, {useState} from "react"
 
-import {ScrollView,FlatList} from "react-native"
+import {ScrollView, FlatList} from "react-native"
 import FeedItem from "./FeedItem"
-import database, { firebase } from '@react-native-firebase/database';
+import database, {firebase} from '@react-native-firebase/database';
 
-function feed(){
+function feed() {
 
-    const [FeedList,setFeedList] = useState([<FeedItem key = {1} text = "feed loading" smilies = {0}/>])
-    const [flag,setFlag] = useState(false)
-
-    const insert2feed = ()=>{
-        const key = database().ref('feed').push().key
-
-        const dat ={
-            "id":key,
-            "text":"Peace",
-            "smilies":1
+    const [FeedList, setFeedList] = useState([
+        {
+            text: "feed loading",
+            id: "1"
         }
-        const data ={}
-        data[key]= dat
+    ])
+    const [flag, setFlag] = useState(false)
 
-        database().ref('feed').update(data)
-    }
-    
-    const getData=()=>{
-        if(!flag)
-        database().ref('feed').once('value')
-            .then(dataSnapshot=>{
-                const data =dataSnapshot.val()
-                const dataArray=[]
-                for(let el in data){
-                    dataArray.push(data[el])
-                }
-                console.log(data)
-                
+    const insert2feed = () => {
+        const key = database()
+            .ref('feed')
+            .push()
+            .key
 
-                // console.log(dataList)
-                setFlag(true)
-                setFeedList(dataArray)
-            })
+        const dat = {
+            "id": key,
+            "text": "Peace",
+            "smilies": 1
+        }
+        const data = {}
+        data[key] = dat
+
+        database()
+            .ref('feed')
+            .update(data)
     }
+
+    const getData = () => {
+        if (!flag) 
+            database()
+                .ref('feed')
+                .once('value')
+                .then(dataSnapshot => {
+                    const data = dataSnapshot.val()
+                    const dataArray = []
+                    for (let el in data) {
+                        dataArray.push(data[el])
+                    }
+                    console.log(data)
+
+                    // console.log(dataList)
+                    setFlag(true)
+                    setFeedList(dataArray)
+                })
+        }
     getData()
-   return(
-    <FlatList
-        data = {FeedList}
-        renderItem={({item})=><FeedItem key = {item.id} text = {item.text} smilies = {item.smilies}/>}
-        keyExtractor={item=>item.id}
-        >
-    </FlatList>)
+    return (
+        <FlatList
+            data={FeedList}
+            renderItem={({item}) =>< FeedItem key = {
+                item.id
+            }
+            id = {
+                item.id
+            }
+            text = {
+                item.text
+            }
+            smilies = {
+                item.smilies
+            } />}
+            keyExtractor={item => item
+                .id
+                .toString()}></FlatList>
+    )
 }
 
 export default feed
