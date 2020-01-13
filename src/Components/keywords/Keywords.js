e2ed4947c229c64ad98ca691e92dff4b0e9e8ab7
aakash <p.saiaakash518@gmail.com>
2020-01-13 13:35:49 +0530

    switched to other api

--- src/Components/keywords/Keywords.js
+++ src/Components/keywords/Keywords.js
@@ -1,20 +1,14 @@
+export default async function Keywords(text) {
+    const axios = require('axios')
 
-export default async function Keywords(text){
-
-   
     const body = new FormData();
     body.append('text', text);
     body.append('api_key', 'fVZe4uwT81OTqfZxAK75VpVHhWMLITn9KBdJWcVy1w0');
 
-   let keywordsJson=  fetch('https://apis.paralleldots.com/v4/keywords', {
-      body,
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      method: 'POST',
-    })
-      .then(res => res.json())
-      
+    let keywordsJson = await fetch(`https://api.dandelion.eu/datatxt/nex/v1/?min_confidence=0.2&text=${text}&token=4873e0b97e264991852a8935ee924fa2&epsilon=0.5`)
+    .then(res => res.json())
 
-    return(keywordsJson)
+    console.log(keywordsJson)
+
+    return (keywordsJson)
 }
